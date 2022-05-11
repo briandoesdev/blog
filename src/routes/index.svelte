f4b0656b16e0e683d35602bb836927069b2f7695
briandoesdev <brian.heidrich@icloud.com>
2022-05-10 20:47:11 -0500

    test deploy

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Sorry, I'm working on it!</h1>
+<p>Site is being made using SvelteKit and GraphCMS. Learn more about me via <a href="https://github.com/briandoesdev">Github</a> or <a href="https://twitter.com/briandoesdev">Twitter</a>!</p>
