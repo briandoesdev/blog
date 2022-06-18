04d0b9da80e82b99629a82398dcfd934ce19205f
briandoesdev <brian.heidrich@icloud.com>
2022-06-17 23:42:45 -0500

    working version of query

--- src/routes/post/[slug].svelte
+++ src/routes/post/[slug].svelte
@@ -1,59 +1,98 @@
 <script context="module">
   // @ts-nocheck
+  export const prerender = false;
+  
+  export const load = async({ params: { slug } }) => {
+    const postSlug = slug;
+    console.log(`post slug: ${slug}`) ;
 
-  import { gql, GraphQLClient } from 'graphql-request'
-  export const prerender = true;
+    const query = await fetch(
+      'https://api-us-east-1.graphcms.com/v2/cl1h36zti4zsq01xfgq4m6f4c/master', {
+      method: 'POST',
+      body: JSON.stringify({
+        query: `query Post($postSlug: String!) {
+                  post(where: { urlSlug: $postSlug }) {
+                    postHeaderImage {
+                      url
+                    }
+                    title
+                    disclaimer
+                    disclaimerType
+                    content
+                    tags
+                    postCreatedAt
+                    postModifiedAt
+                    urlSlug
+                  }
+                }`,
+                variables: { postSlug }
+      })
+    });
 
-  export async function load({ params }) {
-    const postSlug = params.slug;
-    console.log(`slug: ${postSlug}`)
-    const graphcms = new GraphQLClient(
-      import.meta.env.VITE_GRAPHCMS_URL,
-      {
-        headers: {},
+    if (query.ok) {
+      const post = await query.json();
+      return {
+        props: {
+          post,
+        },
       }
-    )
-
-    const query = gql`
-      query Post($postSlug: String!) {
-        post(where: { urlSlug: $postSlug }) {
-          postHeaderImage {
-            url
-          }
-          title
-          disclaimer
-          disclaimerType
-          content
-          tags
-          postCreatedAt
-          postModifiedAt
-          urlSlug
-        }
-      }
-    `
-
-    const variables = {
-      postSlug: postSlug,
-    }
-
-
-    const { blogPost } = await graphcms.request(query, variables)
-    console.log(`resp: ${blogPost}`)
-
-    return {
-      props: {
-        blogPost,
-      },
     }
   }
+
+  // import { gql, GraphQLClient } from 'graphql-request'
+  // export const prerender = true;
+
+  // export async function load({ params }) {
+  //   const postSlug = params.slug;
+  //   console.log(`slug: ${postSlug}`)
+  //   const graphcms = new GraphQLClient(
+  //     import.meta.env.VITE_GRAPHCMS_URL,
+  //     {
+  //       headers: {},
+  //     }
+  //   )
+
+  //   const query = gql`
+  //     query Post($postSlug: String!) {
+  //       post(where: { urlSlug: $postSlug }) {
+  //         postHeaderImage {
+  //           url
+  //         }
+  //         title
+  //         disclaimer
+  //         disclaimerType
+  //         content
+  //         tags
+  //         postCreatedAt
+  //         postModifiedAt
+  //         urlSlug
+  //       }
+  //     }
+  //   `
+
+  //   const variables = {
+  //     postSlug: postSlug,
+  //   }
+
+
+  //   const { blogPost } = await graphcms.request(query, variables)
+  //   console.log(`resp: ${blogPost}`)
+
+  //   return {
+  //     props: {
+  //       blogPost,
+  //     },
+  //   }
+  // }
 </script>
 
 <script>
   // @ts-nocheck
-  export let blogPost
+  export let post
 </script>
 
-<ul>
+<pre>{JSON.stringify(post, null, 2)}</pre>
+<!-- <ul>
   <li>
     {#if blogPost.postHeaderImage != null}
       <img src="{blogPost.postHeaderImage.url}" alt="{blogPost.title}" />
@@ -69,4 +108,4 @@
     <p>{blogPost.postModifiedAt}</p>
     <p>{blogPost.urlSlug}</p>
   </li>
-</ul>
+</ul> -->
