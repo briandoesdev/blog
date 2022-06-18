94812e6d5f4ccb34c2984eddade83a84b70bdde8
briandoesdev <brian.heidrich@icloud.com>
2022-06-17 23:45:16 -0500

    re-added Graphql imports

--- src/routes/post/[slug].svelte
+++ src/routes/post/[slug].svelte
@@ -1,89 +1,50 @@
 <script context="module">
   // @ts-nocheck
   export const prerender = false;
-  
-  export const load = async({ params: { slug } }) => {
-    const postSlug = slug;
-    console.log(`post slug: ${slug}`) ;
 
-    const query = await fetch(
-      'https://api-us-east-1.graphcms.com/v2/cl1h36zti4zsq01xfgq4m6f4c/master', {
-      method: 'POST',
-      body: JSON.stringify({
-        query: `query Post($postSlug: String!) {
-                  post(where: { urlSlug: $postSlug }) {
-                    postHeaderImage {
-                      url
-                    }
-                    title
-                    disclaimer
-                    disclaimerType
-                    content
-                    tags
-                    postCreatedAt
-                    postModifiedAt
-                    urlSlug
-                  }
-                }`,
-                variables: { postSlug }
-      })
-    });
+  import { gql, GraphQLClient } from 'graphql-request'
 
-    if (query.ok) {
-      const post = await query.json();
-      return {
-        props: {
-          post,
-        },
+  export async function load({ params }) {
+    const postSlug = params.slug;
+    console.log(`slug: ${postSlug}`)
+    const graphcms = new GraphQLClient(
+      import.meta.env.VITE_GRAPHCMS_URL,
+      {
+        headers: {},
       }
+    )
+
+    const query = gql`
+      query Post($postSlug: String!) {
+        post(where: { urlSlug: $postSlug }) {
+          postHeaderImage {
+            url
+          }
+          title
+          disclaimer
+          disclaimerType
+          content
+          tags
+          postCreatedAt
+          postModifiedAt
+          urlSlug
+        }
+      }
+    `
+
+    const variables = {
+      postSlug: postSlug,
+    }
+
+
+    const { post } = await graphcms.request(query, variables)
+
+    return {
+      props: {
+        post,
+      },
     }
   }
-
-  // import { gql, GraphQLClient } from 'graphql-request'
-  // export const prerender = true;
-
-  // export async function load({ params }) {
-  //   const postSlug = params.slug;
-  //   console.log(`slug: ${postSlug}`)
-  //   const graphcms = new GraphQLClient(
-  //     import.meta.env.VITE_GRAPHCMS_URL,
-  //     {
-  //       headers: {},
-  //     }
-  //   )
-
-  //   const query = gql`
-  //     query Post($postSlug: String!) {
-  //       post(where: { urlSlug: $postSlug }) {
-  //         postHeaderImage {
-  //           url
-  //         }
-  //         title
-  //         disclaimer
-  //         disclaimerType
-  //         content
-  //         tags
-  //         postCreatedAt
-  //         postModifiedAt
-  //         urlSlug
-  //       }
-  //     }
-  //   `
-
-  //   const variables = {
-  //     postSlug: postSlug,
-  //   }
-
-
-  //   const { blogPost } = await graphcms.request(query, variables)
-  //   console.log(`resp: ${blogPost}`)
-
-  //   return {
-  //     props: {
-  //       blogPost,
-  //     },
-  //   }
-  // }
 </script>
 
 <script>
@@ -91,21 +52,21 @@
   export let post
 </script>
 
-<pre>{JSON.stringify(post, null, 2)}</pre>
-<!-- <ul>
+<!-- <pre>{JSON.stringify(post, null, 2)}</pre> -->
+<ul>
   <li>
-    {#if blogPost.postHeaderImage != null}
-      <img src="{blogPost.postHeaderImage.url}" alt="{blogPost.title}" />
+    {#if post.postHeaderImage != null}
+      <img src="{post.postHeaderImage.url}" alt="{post.title}" />
     {/if}
-    <a href="/post/{blogPost.slug}">{blogPost.title}</a>
-    <p>{blogPost.disclaimer}</p>
-    <p>{blogPost.disclaimerType}</p>
-    <p>{blogPost.content}</p>
-    {#each blogPost.tags as tag}
+    <a href="/post/{post.slug}">{post.title}</a>
+    <p>{post.disclaimer}</p>
+    <p>{post.disclaimerType}</p>
+    <p>{post.content}</p>
+    {#each post.tags as tag}
     <p>#{tag}</p>
     {/each}
-    <p>{blogPost.postCreatedAt}</p>
-    <p>{blogPost.postModifiedAt}</p>
-    <p>{blogPost.urlSlug}</p>
+    <p>{post.postCreatedAt}</p>
+    <p>{post.postModifiedAt}</p>
+    <p>{post.urlSlug}</p>
   </li>
-</ul> -->
+</ul>
